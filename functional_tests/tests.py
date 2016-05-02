cebc14fa3a54aa8a9575004401ae276fb319f838
Matt Morgan <matt.morgan@ithaka.org>
2016-05-02 09:38:05 -0400

    Add new URL and view for adding items to existing lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,6 +2,7 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
 class NewVisitorTest(LiveServerTestCase):
     
     def setUp(self):
