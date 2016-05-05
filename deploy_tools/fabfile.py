11d0de88683366db8133949ef734ae979f92c93c
Matt Morgan <matt.morgan@ithaka.org>
2016-05-05 08:59:18 -0400

    Complete auto-deploy fabric script

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -26,6 +26,37 @@ def _get_latest_source(source_folder):
     current_commit = local('git log -n 1 --format=%H', capture=True)
     run('cd %s && git reset --hard %s' % (source_folder, current_commit))
     
+def _update_settings(source_folder, site_name):
+    settings_path = source_folder + '/superlists/settings.py'
+    sed(settings_path, 'DEBUG=True', 'DEBUG=False')
+    sed(settings_path,
+       'ALLOWED_HOSTS =.+$',
+       'ALLOWED_HOSTS = ["%s"]' % (site_name)
+    )
+    secret_key_file = source_folder + '/superlists/secret_key.py'
+    if not exists(secret_key_file):
+        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+        key = ''.join(random.SystemRandom()).choice(chars) for _ in range(50))
+        append(secret_key_file, "SECRET_KEY is '%s'" % (key,))
+    append(settings_path, '\nfrom .secret_key import SECRET_KEY')
+    
+def _update_virtualenv(source_folder):
+    virtualenv_folder = source_folder +'/../virtualenv'
+    if not exists(virtualenv_folder + '/bin/pip'):
+        run('virtualenv --python=python3 %s' % (virtualenv_folder))
+    run('%2/bin/pip install -r %s/requirements.txt' % (
+        virtualenv_folder, source_folder
+    ))
+    
+def _update_static_files(source_folder):
+    run('cd %s && ../virtualenv/bin/python3 manage.py collectstatic --noinput' % (
+        source_folder
+    ))
+    
 
-        
+def _update_database(source_folder):
+    run('cd %s && ../virtualenv/bin/python3 manage.py migrate --noinput' % (
+        source_folder
+    ))
+    
     
