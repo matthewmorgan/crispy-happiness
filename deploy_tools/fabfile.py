bd6d91c8541f6d3bd7ef98c21d0dfe7a0cb6d567
Matt Morgan <matt.morgan@ithaka.org>
2016-05-05 08:41:33 -0400

    WIP deploy file

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,31 @@
+from fabric.contrib.files import append, exists, sed
+from fabric.api import env, local, run
+import random
+
+REPO_URL = 'https://github.com/matthewmorgan/crispy-happiness.git'
+
+def deploy():
+    site_folder = '/home/%s/sites/%s' % (env.user, env.host)
+    source_folder = site_folder +'/source'
+    _create_directory_structure_if_necessary(site_folder)
+    _get_latest_course(source_folder)
+    _update_settings(source_folder, env.host)
+    _update_virtualenv(source_folder)
+    _update_static_files(source_folder)
+    _update_database(source_folder)
+    
+def _create_directory_structure_if_necessary(site_folder):
+    for subfolder in ('database', 'static', 'virtualenv', 'source'):
+        run('mkdir -p %s/%s' % (site_folder, subfolder))
+
+def _get_latest_source(source_folder):
+    if exists(source_folder + '/.git'):
+        run('cd %s && git fetch' % (source_folder))
+    else:
+        run('git clone %s %s' % (REPO_URL, source_folder))
+    current_commit = local('git log -n 1 --format=%H', capture=True)
+    run('cd %s && git reset --hard %s' % (source_folder, current_commit))
+    
+
+        
+    
