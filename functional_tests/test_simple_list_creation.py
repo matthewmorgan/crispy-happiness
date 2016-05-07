1de810eac22897bd0bad21eb8dc918a9c4d2cf46
Matthew Morgan <matt@dulcetsoftware.com>
2016-05-07 15:14:11 -0400

    Fix bug in template with missing trailing slash on URL

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -31,7 +31,7 @@ class NewVisitorTest(FunctionalTest):
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         edith_list_url = self.browser.current_url
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        # self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
@@ -40,6 +40,9 @@ class NewVisitorTest(FunctionalTest):
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        # print([row.text for row in rows])
 
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
@@ -48,6 +51,7 @@ class NewVisitorTest(FunctionalTest):
         #Francis, a new user, comes to the site
         ##We use a new browser session to make sure that none of Edith's information is coming
         ##through from cookies etc
+
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
