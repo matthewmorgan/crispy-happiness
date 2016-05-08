cece969f69818a8d35feb4a63c1874218fad8abd
Matthew Morgan <matt@dulcetsoftware.com>
2016-05-08 14:22:11 -0400

    Enforce unique items at form validation

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
 
 from lists.models import *
-from lists.forms import EMPTY_LIST_ERROR, ItemForm
+from lists.forms import (
+    EMPTY_LIST_ERROR, DUPLICATE_ITEM_ERROR,
+    ItemForm, ExistingListItemForm
+)
 
 class ItemFormTest(TestCase):
 
@@ -18,13 +21,39 @@ class ItemFormTest(TestCase):
             [EMPTY_LIST_ERROR]
         )
 
-    def test_form_save_hadles_saving_to_a_list(self):
+    def test_form_save_handles_saving_to_a_list(self):
         list_ = List.objects.create()
         form = ItemForm(data={'text': 'do me'})
         new_item = form.save(for_list=list_)
         self.assertEqual(new_item, Item.objects.first())
         self.assertEqual(new_item.text, 'do me')
         self.assertEqual(new_item.list, list_)
+
+class ExistingListItemFormTest(TestCase):
+
+    def test_form_renders_item_text_input(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_)
+        self.assertIn('placeholder="Enter a to-do item"', form.as_p())
+
+    def test_form_validation_for_blank_items(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [EMPTY_LIST_ERROR])
+
+    def test_form_validation_for_duplicate_items(self):
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text='no twins!')
+        form = ExistingListItemForm(for_list=list_, data={'text': 'no twins!'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [DUPLICATE_ITEM_ERROR])
+
+    def test_form_save(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={'text': 'hi'})
+        new_item = form.save()
+        self.assertEqual(new_item, Item.objects.all()[0])
 
 
 
--- lists/views.py
+++ lists/views.py
@@ -1,36 +1,37 @@
-from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
 
-from lists.forms import ItemForm
+from lists.forms import ItemForm, ExistingListItemForm
 from lists.models import Item, List
+
 
 def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
 
+
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    form = ItemForm()
+    form = ExistingListItemForm(for_list=list_)
 
     if request.method == 'POST':
-        form = ItemForm(data=request.POST)
+        form = ExistingListItemForm(for_list=list_, data=request.POST)
         if form.is_valid():
-            form.save(for_list=list_)
+            form.save()
             return redirect(list_)
 
     return render(
         request, 'list.html',
         {
-        'list': list_,
-        'form': form
+            'list': list_,
+            'form': form
         }
     )
+
 
 def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
-        list_=List.objects.create()
+        list_ = List.objects.create()
         form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'home.html', {"form": form})
-
